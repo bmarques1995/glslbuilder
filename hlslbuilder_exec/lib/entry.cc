39788efc4b7087384fea2ebee6ee7e8fb114a435
Bruno S Marques <brunosmarques1995@hotmail.com>
2023-11-20 12:36:28 -0300

    Added console system

--- hlslbuilder_exec/lib/entry.cc
+++ hlslbuilder_exec/lib/entry.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "Console.hh"
 
 int main()
 {
-	std::cout << "Hello NMake\n";
+	HLSLBuilderCLI::Console::Init();
+	HLSLBuilderCLI::Console::Log("Hello HLSLBuilder");
+	HLSLBuilderCLI::Console::End();
 	return 0;
 }
